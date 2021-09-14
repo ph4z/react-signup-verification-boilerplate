4221f3ba666aa66fd763da9ed2a8738ae256736b
ph4z <pierrick@poseidon-networks.com>
2021-09-14 01:07:26 +0000

    Add terra balance

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { accountService } from '@/_services';
+import { AnchorEarn, CHAINS, NETWORKS, DENOMS } from '@anchor-protocol/anchor-earn';
 
 function Home() {
     const user = accountService.userValue;
-    
+
+    const [balanceInfo, setBalanceInfo] = useState([])
+  
+    useEffect( () => {
+  
+      async function fetchData() {
+        const anchorEarn = new AnchorEarn({
+          chain: CHAINS.TERRA,
+          network: NETWORKS.COLUMBUS_4,
+          address: accountService.userValue.terrawal
+  
+        });
+  
+        const bInfo = await anchorEarn.balance({
+          currencies: [DENOMS.UST]
+        });
+  
+        setBalanceInfo(bInfo);
+      }
+  
+      fetchData();
+    }, []);
+
+
     return (
         <div className="p-4">
             <div className="container">
                 <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React & JWT!!</p>
+                <p>Your Terra address: {user.terrawal}</p>
+                <p>Your account balance: {balanceInfo.total_account_balance_in_ust}</p>
+                <p>Your deposit balance: {balanceInfo.total_deposit_balance_in_ust}</p>
+
             </div>
         </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { render } from 'react-dom';
